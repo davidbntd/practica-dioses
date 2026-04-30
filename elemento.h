00f58735f75ffaaf2e20ffefa89ad1f2ec669053
davidbntd <d.benitez.2025@alumnos.urjc.es>
2026-04-30 12:22:03 +0200

    Create elemento.h

---
+++ elemento.h
@@ -0,0 +1,13 @@
+//
+// Created by d.benitez.2025 on 27/02/2026.
+//
+
+#ifndef ELEMENTO_H
+#define ELEMENTO_H
+typedef  char telemento [100];
+void crearElemento (telemento* pe);
+void asignarElemento (telemento* pe, telemento e);
+void mostrarElemento (telemento e);
+int compararElemento (telemento e1, telemento e2);
+
+#endif //ELEMENTO_H
